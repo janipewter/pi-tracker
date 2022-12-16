696f616f28e2c9dc4f5fed144855fcc776c49a70
Jani Pewter <jpe@pml.ac.uk>
2022-12-16 12:19:21 +0000

    updates

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import serial
 import time
 import string
 import pynmea2
+import argparse
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.exceptions import PubNubException
@@ -17,26 +18,44 @@ pnconfig.subscribe_key = config.subscribe_key
 pnconfig.publish_key = config.publish_key
 pnconfig.uuid = config.uuid
 pnconfig.ssl = False
-
 pubnub = PubNub(pnconfig)
 pubnub.subscribe().channels(pnChannel).execute()
 
-while True:
-	gpsd.connect()
-	packet = gpsd.get_current()
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--frequency", required=False, type=int, default=5, choices=range(1,601), metavar="[1-600]",
+                    help="number of seconds between logs (default is 5)")
+args = parser.parse_args()
 
-	if packet.time and packet.lat and packet.lon:
-		try:
-			envelope = pubnub.publish().channel(pnChannel).message({
-			"lat":packet.lat,
-			"lng":packet.lon
-			}).sync()
-			print(packet.time, "timetoken", envelope.result.timetoken, "success", packet.position())
-		except PubNubException as e:
-			handle_exception(e)
-			pass
-	else:
-		print("No GPS fix")
-		continue
 
-	time.sleep(5)
+def main():
+	print("Running with", vars(args)["frequency"], "second sleep between position sends")
+	while True:
+		gpsd.connect()
+		packet = gpsd.get_current()
+
+		if packet.time and packet.lat and packet.lon:
+			while True:
+				try:
+					envelope = pubnub.publish().channel(pnChannel).message({
+					"lat":packet.lat,
+					"lng":packet.lon
+					}).sync()
+					print(packet.time, "timetoken", envelope.result.timetoken, "success", packet.position())
+					time.sleep(float(vars(args)["frequency"])) # sleep for set frequency
+				except PubNubException as e:
+					PubNubException.handle_exception(e)
+					print("Exception in the pubnub send, hopefully we will retry in 1 second")
+					time.sleep(1)
+					continue
+				else:
+					print("Breaking from internal loop")
+					break
+		else:
+			print("No GPS fix")
+			continue
+
+		
+
+
+if __name__ == "__main__":
+	main()
